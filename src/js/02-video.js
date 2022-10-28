3299f5f4b29fd7d041ccf7ec88d637cffdcd8523
Konrad Włodarczyk <draco320@gmail.com>
2022-10-28 10:24:48 +0200

    Update 02-video.js

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -4,13 +4,15 @@ import Player from "@vimeo/player";
 const iframe = document.querySelector('#vimeo-player');
 const iframePlayer = new Player(iframe);
 
-iframePlayer.setCurrentTime(startPlay);
 
 const startPlay = parseFloat(localStorage.getItem(`videoplayer-current-time`)) || 0;
 
-iframePlayer.on(`timeupdate`, throttle(secondsPlay, 1000));
+
+iframePlayer.setCurrentTime(startPlay);
+
 
 function secondsPlay(data) {
     localStorage.setItem(`videoplayer-current-time`, data.seconds);
     console.log(`Time update: ` + localStorage.getItem(`videoplayer-current-time`));
 }
+iframePlayer.on(`timeupdate`, throttle(secondsPlay, 1000));
